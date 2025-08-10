cc26a301e996f8bcbc5029608d51f305adf5757a
dangph12 <dangphhe181217@fpt.edu.vn>
2025-08-10 22:00:51 +0700

    feat: add sort import deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
@@ -10,6 +11,9 @@ export default tseslint.config(
   },
   {
     files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      'simple-import-sort': simpleImportSort
+    },
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
@@ -25,7 +29,9 @@ export default tseslint.config(
       '@typescript-eslint/no-inferrable-types': 'off',
       'no-console': 'off',
       'no-empty-function': 'off',
-      'react-hooks/exhaustive-deps': 'off'
+      'react-hooks/exhaustive-deps': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
     }
   },
   {
