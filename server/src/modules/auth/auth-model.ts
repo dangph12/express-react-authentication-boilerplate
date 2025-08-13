3e0a726d0b8a558a97dff2a910eeaaf5506279da
dangph12 <dangphhe181217@fpt.edu.vn>
2025-08-13 17:28:01 +0700

    fix: make provider id optional

--- server/src/modules/auth/auth-model.ts
+++ server/src/modules/auth/auth-model.ts
@@ -3,12 +3,12 @@ import { model, Schema } from 'mongoose';
 import { IAuth } from './auth-type';
 
 // One user can have multiple auth accounts
-// For local authentication, provider will be 'local' and providerId will be empty string
+// For local authentication, provider will be 'local'
 const AuthSchema = new Schema<IAuth>(
   {
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     provider: { type: String, required: true },
-    providerId: { type: String, required: true },
+    providerId: { type: String },
     localPassword: { type: String },
     verifyAt: { type: Date, default: null }
   },
