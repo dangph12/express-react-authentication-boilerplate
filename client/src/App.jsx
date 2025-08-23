021557842b273c5f5f3b1c3c4f85bf0d60b22dd8
dangph12 <dangphhe181217@fpt.edu.vn>
2025-08-23 10:26:04 +0700

    feat: init auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,31 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RouterProvider } from 'react-router';
 
+import { Spinner } from '~/components/ui/spinner';
 import router from '~/routes/router';
+import { initializeAuth } from '~/store/features/authSlice';
 
 const App = () => {
   const theme = useSelector(state => state.theme.value);
+  const { loading } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     document.documentElement.setAttribute('class', theme);
   }, [theme]);
+
+  useEffect(() => {
+    dispatch(initializeAuth());
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center min-h-screen'>
+        <Spinner size='lg' />
+      </div>
+    );
+  }
 
   return <RouterProvider router={router} />;
 };
