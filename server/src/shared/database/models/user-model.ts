2fe0229e734009283327cb2bf283e87215df01bb
dangph12 <dangphhe181217@fpt.edu.vn>
2025-11-26 20:07:35 +0700

    fix: add enum

--- server/src/shared/database/models/user-model.ts
+++ server/src/shared/database/models/user-model.ts
@@ -2,14 +2,21 @@ import mongoose, { type PaginateModel, Schema } from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 import type { User } from '~/entities/user';
+import { GENDER_VALUES } from '~/shared/constants/gender';
+import { Role, ROLE_VALUES } from '~/shared/constants/role';
 
 const userSchema = new Schema<User>(
   {
     email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     avatar: { type: String, default: '' },
-    gender: { type: String },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    gender: { type: String, enum: GENDER_VALUES },
+    role: {
+      type: String,
+      enum: ROLE_VALUES,
+      required: true,
+      default: Role.USER
+    },
     dob: { type: Date },
     isActive: { type: Boolean, default: true }
   },
