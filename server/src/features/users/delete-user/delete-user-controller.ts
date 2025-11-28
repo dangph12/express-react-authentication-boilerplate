b4216a1fa2750beae1f88d99ff8addd641f38950
dangph12 <dangphhe181217@fpt.edu.vn>
2025-11-28 10:40:26 +0700

    fix: handle delete own user

--- server/src/features/users/delete-user/delete-user-controller.ts
+++ server/src/features/users/delete-user/delete-user-controller.ts
@@ -8,6 +8,14 @@ export const DeleteUserController = {
   deleteUser: async (req: Request, res: Response) => {
     const id = req.params.id;
 
+    const currentUserId = req.user?._id.toString();
+
+    if (id === currentUserId) {
+      return res
+        .status(400)
+        .json(ApiResponse.failed('Admin cannot delete own account'));
+    }
+
     const result = await DeleteUserService.deleteUser(id);
 
     res
