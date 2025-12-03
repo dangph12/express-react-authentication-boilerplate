8a17b2a2667bfd95dcd913517f2497d7d9c1652e
dangph12 <dangphhe181217@fpt.edu.vn>
2025-12-03 17:58:53 +0700

    feat: sign up page

---
+++ client/src/constants/gender.js
@@ -0,0 +1,11 @@
+export const Gender = {
+  MALE: 'male',
+  FEMALE: 'female',
+  OTHER: 'other'
+};
+
+export const GENDER_OPTIONS = [
+  { value: Gender.MALE, label: 'Male' },
+  { value: Gender.FEMALE, label: 'Female' },
+  { value: Gender.OTHER, label: 'Other' }
+];
