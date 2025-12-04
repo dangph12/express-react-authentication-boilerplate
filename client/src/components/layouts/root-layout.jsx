8e40b2e406642c38b67414f0495890b3a6cc385f
dangph12 <dangphhe181217@fpt.edu.vn>
2025-12-04 11:15:46 +0700

    chore: remove unused redux logic

--- client/src/components/layouts/root-layout.jsx
+++ client/src/components/layouts/root-layout.jsx
@@ -1,22 +1,11 @@
 import { useTheme } from 'next-themes';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router';
 
 import Header from '~/components/header';
 import { Toaster } from '~/components/ui/sonner';
-import { fetchAvatar } from '~/store/features/avatar-slice';
 
 const RootLayout = () => {
-  const dispatch = useDispatch();
-  const { user } = useSelector(state => state.auth);
   const { theme } = useTheme();
-
-  useEffect(() => {
-    if (user) {
-      dispatch(fetchAvatar());
-    }
-  }, [dispatch, user]);
 
   return (
     <div className='min-h-screen flex flex-col'>
