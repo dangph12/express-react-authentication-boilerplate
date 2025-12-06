c9d5d36f1d7e41a3011dd16613cd5a23f413f9d7
dangph12 <dangphhe181217@fpt.edu.vn>
2025-12-06 09:55:09 +0700

    refactor: user query param building logic

--- client/src/features/users/view-users/api/view-users.js
+++ client/src/features/users/view-users/api/view-users.js
@@ -1,22 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 
 import apiClient from '~/lib/api-client';
+import { buildQueryParams } from '~/lib/build-query-params';
 import { QUERY_KEYS } from '~/lib/query-keys';
 
 const fetchUsers = async params => {
-  const searchParams = new URLSearchParams();
-
-  if (params.page) searchParams.set('page', params.page.toString());
-  if (params.limit) searchParams.set('limit', params.limit.toString());
-  if (params.sort) searchParams.set('sort', params.sort);
-  if (params.name) searchParams.set('name', `/${params.name}/i`);
-  if (params.gender?.length) {
-    params.gender.forEach(g => searchParams.append('gender', g));
-  }
-  if (params.role?.length) {
-    params.role.forEach(r => searchParams.append('role', r));
-  }
-
+  const searchParams = buildQueryParams(params, ['name']);
   const response = await apiClient.get(`/api/users?${searchParams.toString()}`);
   return response.data.data;
 };
